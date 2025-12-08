6645ca8ea72b2803751228208491bfc03de04c87
Besu <maluworkq@gmail.com>
2025-12-08 20:56:19 -0300

    att

--- src/Model/configFirebase.js
+++ src/Model/configFirebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB31IZTdVVqvpR42akBqg1MethvAozL_20",
   authDomain: "projetopwa-1c79a.firebaseapp.com",
   projectId: "projetopwa-1c79a",
-  storageBucket: "projetopwa-1c79a.appspot.com", // ✅ corrigido
+  storageBucket: "projetopwa-1c79a.appspot.com",
   messagingSenderId: "262652544848",
   appId: "1:262652544848:web:da4b58af50dbdfccd28e55",
   measurementId: "G-SQJPC0BCWX"
