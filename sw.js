0b733091bdb10e5895d0d48ed4be02ee49b28359
Besu <maluworkq@gmail.com>
2025-12-08 05:00:35 -0300

    att

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ const urlsToCache = [
   './src/View/Atualizar.html',
   './src/View/PaginaInicial.html',
   './src/View/Usuario.html',
-  'https://bbesu2.github.io/PWAProjeto/icons/icon-48.png',
-  'https://bbesu2.github.io/PWAProjeto/icons/icon-256.png'
+  'https://github.com/Bbesu2/PWAProjeto/blob/main/icons/icon-48.png',
+  'https://github.com/Bbesu2/PWAProjeto/blob/main/icons/icon-256.png'
 ];
 
 self.addEventListener('install', (event) => {
