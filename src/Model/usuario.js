32699192d491cccd4608dc015c9b98b6593cc44b
Besu <maluworkq@gmail.com>
2025-12-11 15:45:54 -0300

    ãtt

--- src/Model/usuario.js
+++ src/Model/usuario.js
@@ -1,4 +1,4 @@
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
     import { getAuth,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
     import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
   
@@ -6,7 +6,7 @@
     apiKey: "AIzaSyB31IZTdVVqvpR42akBqg1MethvAozL_20",
     authDomain: "projetopwa-1c79a.firebaseapp.com",
     projectId: "projetopwa-1c79a",
-    storageBucket: "projetopwa-1c79a.firebasestorage.app",
+    storageBucket: "projetopwa-1c79a.appspot.com",
     messagingSenderId: "262652544848",
     appId: "1:262652544848:web:da4b58af50dbdfccd28e55",
     measurementId: "G-SQJPC0BCWX"
