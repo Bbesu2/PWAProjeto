eb4acd2758ea79b994a7fd02263b6ef4e4c73a5b
Besu <maluworkq@gmail.com>
2025-12-12 00:17:58 -0300

    Update user-delete.js

--- src/Controller/user-delete.js
+++ src/Controller/user-delete.js
@@ -9,45 +9,32 @@ import { doc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.6.0/fireba
 onAuthStateChanged(auth, (user) => {
   if (!user) return;
 
-  const deleteBtn = document.getElementById("deleteAccount");
-  if (!deleteBtn) return;
 
-  deleteBtn.addEventListener("click", async () => {
-    const confirmar = confirm("Tem certeza que deseja excluir sua conta?");
-    if (!confirmar) return;
+const deleteBtn = document.getElementById("deleteAccount");
 
+ deleteBtn?.addEventListener("click", async () => {
     try {
       await deleteDoc(doc(db, "users", user.uid));
       await user.delete();
-
       alert("Conta excluída com sucesso!");
       window.location.href = "Cadastro.html";
     } catch (error) {
       console.error("Erro ao excluir:", error);
-
       if (error.code === "auth/requires-recent-login") {
         const senha = prompt("Digite sua senha para confirmar exclusão:");
-        if (!senha) {
-          alert("Exclusão cancelada.");
-          return;
-        }
-
-        try {
+        if (senha) {
           const cred = EmailAuthProvider.credential(user.email, senha);
           await reauthenticateWithCredential(user, cred);
-
-          await deleteDoc(doc(db, "users", user.uid));
           await user.delete();
-
+          await deleteDoc(doc(db, "users", user.uid));
           alert("Conta excluída com sucesso!");
           window.location.href = "Cadastro.html";
-        } catch (err) {
-          console.error("Erro na reautenticação:", err);
-          alert("Erro ao excluir conta: " + err.message);
+        } else {
+          alert("Exclusão cancelada.");
         }
       } else {
         alert("Erro ao excluir conta: " + error.message);
       }
     }
-  });
+  })
 });
